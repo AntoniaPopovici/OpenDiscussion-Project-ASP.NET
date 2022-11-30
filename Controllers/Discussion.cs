d1767b6c32b1144e808806b0cc4649fd0f79929d
BiancaGabriela06 <bianca-gabriela.asavoaei@s.unibuc.ro>
2022-11-30 22:33:49 +0200

    Am adaugat Controller Discussion

---
+++ Controllers/Discussion.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenDiscussion.Controllers
+{
+    public class Discussion : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
