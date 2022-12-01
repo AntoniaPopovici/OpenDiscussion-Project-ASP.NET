845c5e9c279ea810cc583cc63e77a4e36b5ca7c1
BiancaGabriela06 <bianca-gabriela.asavoaei@s.unibuc.ro>
2022-12-01 20:04:02 +0200

    aDAUGARE MODELE SI CONTROLLERE

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenDiscussion.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: vizualizarea tuturor categoriilor
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// GET :vizualizarea unei categorii
+         
+        public IActionResult Show(int id)
+        {
+            return View();
+        }
+
+        // GET : editarea unei categorii
+        public IActionResult Edit(int ID)
+        {
+            return View();
+        }
+
+
+        // POST: se trimit modificarile la server si se stocheaza
+        [HttpPost]
+        public IActionResult Edit(Student ID)
+        {
+            // cod modificare date categorie
+            // se redirectioneaza browser-ul catre categorie editat
+            return Redirect("/Students/Edit" + ID);
+            //return RedirectToRoute("students_show", new { id = ID
+        });
+ }
+}
+}
---
+++ Models/Discussion.cs
@@ -0,0 +1,6 @@
+﻿namespace OpenDiscussion.Models
+{
+    public class Discussion
+    {
+    }
+}
